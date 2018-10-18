ad86383c234e400cc2b53ead63398644a1490c79
xyn11 <xuyinan11@gmail.com>
2018-10-17 17:34:17 -0700

    add game over check

--- tictactoe.py
+++ tictactoe.py
@@ -3,28 +3,22 @@ import random
 class Board:
     def __init__(self):
         self.squares = [' '] * 9 
-        self.movelist = []
-        self.playeroccupy = set()
-        self.computeroccupy = set()
 
     def playermove(self, n):
         if self.validsquare(n):
             self.squares[n] = 'X'
-            self.movelist.append(n)
-            self.playeroccupy.add(n) 
             return True        
         return False
 
     def validsquare(self, n):
-        if n in self.movelist or n not in range(9):
+        if self.squares[n] != ' ':
             return False
         return True
 
     def randommove(self):
         possiblemove = []
-        movelist = self.movelist
         for i in range(9):
-            if i not in movelist:
+            if self.squares[i] == ' ':
                 possiblemove.append(i)
         if possiblemove != []:
             return random.choice(possiblemove)
@@ -33,19 +27,44 @@ class Board:
     def computermove(self):
         n = self.randommove()
         if self.validsquare(n):
-            self.squares[n] = 'O'
-            self.movelist.append(n)  
-            self.computeroccupy.add(n)     
+            self.squares[n] = 'O'  
+
+    def check_row(self, n):
+        squares = self.squares
+        if squares[3 * n] != ' ':
+            for i in range(1,3):
+                if squares[3 * n + i] != squares[3 * n]:
+                    return False, ' '
+            return True, squares[n * 3]
+        return False, ' '
+
+    def check_column(self, n):
+        squares = self.squares
+        if squares[n] != ' ':
+            for i in range(1, 3):
+                if squares[n + 3 * i] != squares[n]:
+                    return False, ' '
+            return True, squares[n]
+        return False, ' '
+
 
     def win(self):
-        winset = [{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}]
-        for x in winset:
-            if x == (x & self.playeroccupy):
-                print('You win.')
-            if x == (x & self.computeroccupy):
-                print('You lose.')
-        if len(self.movelist) == 9:
-            print('Tie')
+        squares = self.squares
+        for i in range(3):
+            over, winner = self.check_row(i)
+            if over:
+                return over, winner
+            over, winner = self.check_column(i)
+            if over:
+                return over, winner        
+        if squares[0] != ' ' and squares[4] == squares[0] and squares[0] == squares[8]:
+            return True, squares[0]
+        if squares[2] != ' ' and squares[2] == squares[4] and squares[4] == squares[6]:
+            return True, squares[2]
+        for i in range(9):
+            if squares[i] == ' ':
+                return False, ' '
+        return True, ' '
 
     def drawboard(self):
         squares = self.squares
@@ -60,12 +79,21 @@ random.seed(10)
 
 while True:
     entry = input('Please enter a number(from 1-9): ')
-    if game.playermove(int(entry)-1):        
+    if int(entry) not in range(1,10):
+        continue
+    if game.playermove(int(entry)-1):
         game.drawboard()
-#        if game.win():
-#            break
-        game.computermove()
-        game.drawboard()
-#        if game.win():
-#            break
+        over, winner = game.win()
+        if not over:
+            game.computermove()
+            game.drawboard()
+        over, winner = game.win()
+        if over:
+            if winner == 'X':
+                print('You win')
+            elif winner == 'O':
+                print('You lose')
+            else:
+                print('Tie')
+            break
 
